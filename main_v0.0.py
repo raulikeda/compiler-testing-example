76dbb3f0ea0a6df19aeb34dc108d1fb99dd032e3
Raul Ikeda <rauligs@insper.edu.br>
2025-07-23 17:54:48 -0300

    arg and file adjustments

---
+++ main_v0.0.py
@@ -0,0 +1,7 @@
+import sys
+
+if __name__ == "__main__":
+    code = sys.argv[1]
+    #raise Exception("This is a test exception")
+    print(eval(code))
+    
--- test.sh
+++ test.sh
@@ -6,17 +6,28 @@ from compiler_testing_lib.runner import TestRunner
 lang = sys.argv[1] if len(sys.argv) > 1 else 'python'
 
 if lang == 'js':
-    runner = TestRunner(language='python', version='v1.0', max_errors=3, timeout=10, file_extension='py')
+    runner = TestRunner(language='python', version='v2.1', max_errors=3, timeout=10, file_extension='py')
     result = runner.run_tests(command_template='node main.js')
 else:
-    runner = TestRunner(language='python', version='v1.0', max_errors=3, timeout=10, file_extension='py')
-    result = runner.run_tests(command_template='python3 main.py')
+    print(f"Running tests for v0.0...")
+    runner = TestRunner(language='python', version='v0.0', max_errors=3, timeout=10, file_extension='py')
+    result = runner.run_tests(command_template='python3 main_v0.0.py')
 
-if result == "":
-    print("\033[92mAll tests passed!\033[0m")  # Green text
-else:
-    print("\n\033[91mTest Divergences:\033[0m\n")  # Red text
-    print(result)
+    if result == "":
+        print("\033[92mAll tests passed!\033[0m")  # Green text
+    else:
+        print("\n\033[91mTest Divergences:\033[0m\n")  # Red text
+        print(result)
+
+    print(f"Running tests for v2.1...")
+    runner = TestRunner(language='python', version='v2.1', max_errors=3, timeout=10, file_extension='py')
+    result = runner.run_tests(command_template='python3 main_v2.1.py')
+
+    if result == "":
+        print("\033[92mAll tests passed!\033[0m")  # Green text
+    else:
+        print("\n\033[91mTest Divergences:\033[0m\n")  # Red text
+        print(result)
 
 # Optionally, demonstrate running main.py with a test file
 def run_main_with_codefile(codefile):
